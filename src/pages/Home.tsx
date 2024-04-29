241d11dec54b22cea2d82b14018b003c14cbe2b9
yeonna18k <yeonna18k@gmail.com>
2024-04-30 00:20:57 +0900

    feat: Implement grid layout and pagination

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 import { PokemonClient } from "pokenode-ts";
 
@@ -11,23 +15,32 @@ interface PokemonTypes {
   id: number;
   name: string;
   abilities: [];
-  types: [];
+  types: TypesType[];
   sprites: SpritesType;
 }
 interface SpritesType {
   front_default: string;
+}
+interface Type {
+  name: string;
+  url: string;
+}
+interface TypesType {
+  slot: number;
+  type: Type;
 }
 
 const Home = () => {
   const [list, setList] = useState<NamedAPIResource[]>();
   const [data, setData] = useState<PokemonTypes[]>([]);
   const [page, setPage] = useState(1);
+
   console.log(data);
 
   const pokeApi = new PokemonClient();
   const fetchAllPokemon = async () => {
     try {
-      const { results } = await pokeApi.listPokemons(page - 1, page * 10);
+      const { results } = await pokeApi.listPokemons((page - 1) * 20, 20);
       setList(results);
       console.log(results);
       list && getPokemon(list);
@@ -43,26 +56,59 @@ const Home = () => {
     } catch (error) {}
   };
 
+  const handlePrevClick = () => {
+    setList([]);
+    setData([]);
+    setPage((prev) => prev - 1);
+  };
+  const handleNextClick = () => {
+    setList([]);
+    setData([]);
+    setPage((prev) => prev + 1);
+  };
+
   useEffect(() => {
     fetchAllPokemon();
-  }, []);
+  }, [page]);
   useEffect(() => {
     list && getPokemon(list);
   }, [list]);
 
   return (
     <>
-      {data?.map((e, i) => {
-        return (
-          <div key={i}>
-            <div className="name"> {e.id}</div>
-            <div className="name"> {e.name}</div>
-            <img src={e.sprites.front_default} alt={e.sprites.front_default} />
-            <div className="types"></div>
-          </div>
-        );
-      })}
-      <></>
+      <Box>
+        <div>{page}page</div>
+        <Grid container spacing={2}>
+          {data?.map((pokemon, index) => {
+            return (
+              <Grid item xs={12} sm={4} md={3} key={index}>
+                <Box p={2}>
+                  <div className="name">No. {pokemon.id}</div>
+                  <div className="name"> {pokemon.name}</div>
+                  <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                  <div className="types">
+                    {pokemon.types.map((e, i) => {
+                      return (
+                        <Box key={i} component="span" p={1}>
+                          {e.type.name}
+                        </Box>
+                      );
+                    })}
+                  </div>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" onClick={handlePrevClick}>
+          Prev
+        </Button>
+        <Button variant="contained" onClick={handleNextClick}>
+          Next
+        </Button>
+      </Stack>
     </>
   );
 };
