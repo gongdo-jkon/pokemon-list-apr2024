cd30252c45356885c45c72d20e7803f0e54c9e2f
yeonna18k <yeonna18k@gmail.com>
2024-04-30 01:25:02 +0900

    style: Update home page layout and colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,11 +34,14 @@ const Home = () => {
   const [list, setList] = useState<NamedAPIResource[]>();
   const [data, setData] = useState<PokemonTypes[]>([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  console.log(loading);
 
   console.log(data);
 
   const pokeApi = new PokemonClient();
   const fetchAllPokemon = async () => {
+    setLoading(true);
     try {
       const { results } = await pokeApi.listPokemons((page - 1) * 20, 20);
       setList(results);
@@ -46,6 +49,7 @@ const Home = () => {
       list && getPokemon(list);
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   };
   const getPokemon = async (list: NamedAPIResource[]) => {
@@ -53,7 +57,11 @@ const Home = () => {
       const promises = list.map((item) => axios.get(item.url));
       const results = await Promise.all(promises);
       setData((prev) => [...prev, ...results.map((result) => result.data)]);
-    } catch (error) {}
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const handlePrevClick = () => {
@@ -70,31 +78,57 @@ const Home = () => {
   useEffect(() => {
     fetchAllPokemon();
   }, [page]);
+
   useEffect(() => {
-    list && getPokemon(list);
+    if (list && list.length > 0) getPokemon(list);
   }, [list]);
 
   return (
     <>
+      <>{loading ? <>Loading ...</> : <></>}</>
       <Box>
-        <div>{page}page</div>
+        <div>{page} page</div>
         <Grid container spacing={2}>
           {data?.map((pokemon, index) => {
             return (
               <Grid item xs={12} sm={4} md={3} key={index}>
-                <Box p={2}>
-                  <div className="name">No. {pokemon.id}</div>
-                  <div className="name"> {pokemon.name}</div>
+                <Box
+                  sx={{
+                    p: 2,
+                    border: "1px solid black",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Box className="name" sx={{ fontSize: "10px" }}>
+                    No. {pokemon.id}
+                  </Box>
+                  <Box className="name" sx={{ fontSize: "16px" }}>
+                    {" "}
+                    {pokemon.name}
+                  </Box>
                   <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                  <div className="types">
-                    {pokemon.types.map((e, i) => {
+                  <Box
+                    className="types"
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    {pokemon.types.map((type, i) => {
                       return (
-                        <Box key={i} component="span" p={1}>
-                          {e.type.name}
+                        <Box
+                          key={i}
+                          p={1}
+                          mr={1}
+                          sx={{
+                            bgcolor: "gray",
+                            borderRadius: 1.5,
+                            color: "white",
+                          }}
+                        >
+                          {type.type.name}
                         </Box>
                       );
                     })}
-                  </div>
+                  </Box>
                 </Box>
               </Grid>
             );
@@ -102,9 +136,11 @@ const Home = () => {
         </Grid>
       </Box>
       <Stack spacing={2} direction="row">
-        <Button variant="contained" onClick={handlePrevClick}>
-          Prev
-        </Button>
+        {page > 1 ? (
+          <Button variant="contained" onClick={handlePrevClick}>
+            Prev
+          </Button>
+        ) : null}
         <Button variant="contained" onClick={handleNextClick}>
           Next
         </Button>
